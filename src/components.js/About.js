d396a8e5d36adb3fb456aa3b00ff9d77a7521d28
Rony Pablo <ronyalexis0@gmail.com>
2023-04-20 23:22:08 -0700

    typo fix

--- src/components.js/About.js
+++ src/components.js/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 <img src={avatar} alt="avatar" />
                 <div>
                     <h3>About Me!</h3>
-                    <p> Hello! I love to build/design things than live on the internet and help people. My interest in web development began during the pandemic when I saw an opportunity to support small restaurant businesses by developing their websites using Wordpress, setting up online ordering systems, and designing menus that increased customer footfall. This experience sparked my interest in software development, and I decided to enroll in an intensive online coding bootcamp with Coding Dojo.</p>
+                    <p> Hello! I love to build/design things that live on the internet and help people. My interest in web development began during the pandemic when I saw an opportunity to support small restaurant businesses by developing their websites using Wordpress, setting up online ordering systems, and designing menus that increased customer footfall. This experience sparked my interest in software development, and I decided to enroll in an intensive online coding bootcamp with Coding Dojo.</p>
                     <p>When I am not coding, I enjoy watching anime and reading</p>
                 </div>
             </div>
